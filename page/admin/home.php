950d788c535a07762d2a1a034b042cad5166e3e1
rafliiar17 <boozxtr008@gmail.com>
2023-02-25 09:29:15 +0700

    #tambah home

---
+++ page/admin/home.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Selamat Datang</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css" integrity="sha512-qY+0t0J2QjKvna+5bQrOJ/zgmBNRtJ5N5aZus+gRFwEs8O/IS3q80P7MAn9pSgGELfBGW3qDYAX7tzZ2w4u4Rw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <style>
+      body {
+        background-color: #f8f9fa;
+      }
+      .jumbotron {
+        background-color: #ffffff;
+        margin-top: 50px;
+      }
+      .display-4 {
+        font-size: 3rem;
+      }
+      .lead {
+        font-size: 1.5rem;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="container">
+      <div class="jumbotron text-center">
+        <h1 class="display-4">Selamat Datang di Halaman Saya</h1>
+        <p class="lead">SILAHKAN ABSENSI JIKA BELUM ABSEN</p>
+      </div>
+    </div>
+    <!-- Bootstrap JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-4+1XJNziTHf7vM6U+QogiO9H6OY8/3bKV6luKw6p/5j5z5J05p5GmJN9S9ez+Y0Q2Gv+kkKzEnz+4j4nLfQ2PQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.9.3/umd/popper.min.js" integrity="sha512-7VdSNMnsu2HcTlaPb8zMKe3sA1i/Pms/BBJXzYqhOj7NcN2QRYcT+f+c0jMst+ixjCJtALx9Cr0aRGFjKZpEag==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.min.js" integrity="sha512-6zGn6LObP6oYU8gq3Zc/S1CwDrpMNKue5d5z5bssxjM5DRS9RE8mucYI+PWa87wAa4Phdm4RzN/6NvU6QgU+hA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+  </body>
+</html>
